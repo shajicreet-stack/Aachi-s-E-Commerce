dd9bdb83f001a0c706ac4587190bf2b9b9f9b58d
shajicreet-stack <shajicreet@gmail.com>
2026-09-23 00:44:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("Aachi's Foods & Spices Website Loaded");
+// Scroll To Top Button
+
+let topBtn = document.getElementById("topBtn");
+
+window.onscroll = function(){
+
+    if(document.body.scrollTop > 300 || document.documentElement.scrollTop > 300){
+
+        topBtn.style.display = "block";
+
+    }else{
+
+        topBtn.style.display = "none";
+
+    }
+
+}
+
+topBtn.onclick = function(){
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+}
